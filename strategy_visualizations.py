b7cb29f3e2677b489b5b0e21d6a7e17df6b330f4
Marc Harper <marc.harper@gmail.com>
2015-11-10 16:30:59 -0800

    Rewrite strategy visualizations and remove old assets

--- strategy_visualizations.py
+++ strategy_visualizations.py
@@ -2,12 +2,17 @@
 """
 Runs example tournaments using the Axelrod library available at
 https://github.com/Axelrod-Python/Axelrod
+
+Requires python 3.4+
 """
 
 import argparse
-import copy
-import os
+import csv # python 3+ usage
+import itertools
 from operator import itemgetter, attrgetter
+import os
+from pathlib import Path # python 3.4+
+import sys
 
 import numpy
 import matplotlib
@@ -17,9 +22,13 @@ import axelrod
 
 from example_tournaments import axelrod_strategies, ensure_directory
 
+## Python 3.5
+#def ensure_directory(path):
+    #path.mkdir(parents=True, exist_ok=True)
+
+## Commandline arguments
 
 def parse_args():
-
     parser = argparse.ArgumentParser(description="Run Sample Axelrod tournaments")
 
     parser.add_argument(
@@ -48,21 +57,127 @@ def parse_args():
         default=0,
         help='Noise level')
 
+    parser.add_argument(
+        '-d', '--data',
+        action="store_true",
+        help='Generate Data')
+
+
     args = parser.parse_args()
 
     return (args.turns, args.repetitions, args.noise,
-            args.function)
+            args.function, args.data)
+
+# Various helpers
+
+def normalized_name(player):
+    """Normalizes the player name."""
+    player_name = str(player)
+    for char1, char2 in [('/', '-'), ('\\', ''), ('$', '')]:
+        player_name = player_name.replace(char1, char2)
+    return player_name
 
 def game_extremes():
-    """
-    Returns the max and min game matrix values to set the colorbar endpoints.
-    """
+    """Returns the max and min game matrix values to set the colorbar
+    endpoints."""
     game = axelrod.Game()
     scores = game.RPST()
     return min(scores), max(scores)
 
+def unzip(l):
+    """Unpacks a list of tuples pairwise to a lists of lists:
+    [('a', 1), ('b', 2)] becomes [['a', 'b'], [1, 2]]"""
+    return zip(*l)
+
+## Generate match results and cache to CSV
+
+def csv_filename(player, opponent):
+    """Provides a standardized filename for storing and loading match data."""
+    filename = "{}--{}.csv".format(normalized_name(player), normalized_name(opponent))
+    return filename
+
+def load_match_csv(player, opponent):
+    """Loads a cached CSV file. Returns a list of lists of match plays:
+        [
+            [('C', 'D'), ('C', 'C'), ... ],
+            ...
+        ]
+    This function will also attempt to change the order if the data is not found,
+    since swapping player and opponent will only change the order of plays.
+    """
+    reverse = False
+    filename = csv_filename(player, opponent)
+    path = Path("csv") / "matches" / filename
+    # Check to see if data exists for the other permutation
+    if not path.exists():
+        reverse = True
+        filename = csv_filename(opponent, player)
+        path = Path("csv") / "matches" / filename
+        # No match data exists for these two players
+        if not path.exists:
+            raise FileNotFoundError("No match data found")
+    with path.open('r') as csvfile:
+        reader = csv.reader(csvfile)
+        # Separate the plays in each round, handle reversing
+        index0, index1 = 0, 1
+        if reverse:
+            index0, index1 = 1, 0
+        for row in reader:
+            yield [(elem[index0], elem[index1]) for elem in row]
+
+def write_match_to_csv(data, filename, data_directory="csv"):
+    """Takes match data (or a generator) and writes the data to a csv file."""
+    #ensure_directory("csv")
+    path = Path("csv") / "matches" / filename
+    with path.open('w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in data:
+            csv_row = ["".join(element) for element in row]
+            writer.writerow(csv_row)
+
+def generate_match_results(player, opponent, turns=200, repetitions=100,
+                           noise=0):
+    """Generates match date between two players. Yields rows of the form
+    [(C, D), (C, C), ...]."""
+    # Make sure we have two distinct player objects
+    if player == opponent:
+        opponent = opponent.clone()
+    # Set tournament_attributes
+    tournament_attributes = {'length': turns, 'game': axelrod.Game()}
+    player.tournament_attributes = tournament_attributes
+    opponent.tournament_attributes = tournament_attributes
+
+    # Run matches
+    row = []
+    for _ in range(repetitions):
+        # Compute a new match
+        row = []
+        # Reset any accumulated history
+        player.reset()
+        opponent.reset()
+        # Compute rounds and write to CSV
+        for _ in range(turns):
+            player.play(opponent, noise=noise)
+        yield zip(player.history, opponent.history)
+
+def save_all_match_results(players, turns=200, repetitions=100, noise=0):
+    """Caches match results for all pairs of players"""
+    for p1, p2 in itertools.combinations_with_replacement(players, 2):
+        print(p1, p2)
+        # Check if the outcome will be deterministic. If so, only run one repetition
+        if p1.classifier['stochastic'] or p2.classifier['stochastic'] or noise:
+            repetitions_ = repetitions
+        else:
+            repetitions_ = 1
+        data = generate_match_results(p1, p2, turns=turns,
+                                        repetitions=repetitions_, noise=noise)
+        filename = csv_filename(p1, p2)
+        write_match_to_csv(data, filename)
+
+## Classes to reduce the data sets in various ways
 
 class CooperationAggregator(object):
+    """Aggregates the cooperation probability per round over many histories."""
     def __init__(self):
         self.mapping = {'C': 1, 'D': 0}
         self.counts = []
@@ -80,6 +195,8 @@ class CooperationAggregator(object):
 
 
 class OpponentCooperationAggregator(object):
+    """Aggregates the cooperation probability of the opponentper round over
+    many histories."""
     def __init__(self):
         self.mapping = {'C': 1, 'D': 0}
         self.counts = []
@@ -97,6 +214,7 @@ class OpponentCooperationAggregator(object):
 
 
 class ScoreAggregator(object):
+    """Aggregates the mean score per round over many histories."""
     def __init__(self):
         game = axelrod.Game()
         self.mapping = game.scores
@@ -116,6 +234,7 @@ class ScoreAggregator(object):
 
 
 class ScoreDiffAggregator(object):
+    """Aggregates the mean score difference per round over many histories."""
     def __init__(self):
         game = axelrod.Game()
         self.mapping = game.scores
@@ -134,39 +253,89 @@ class ScoreDiffAggregator(object):
         return numpy.array(self.counts) / float(self.rows)
 
 
-def iterate_plays(player, opponents, turns=200, repetitions=50, noise=0, aggClass=None):
-    """Runs many sequences of play to generate play data for computing the
-    average score per turn for each opponent."""
-    tournament_attributes = {
-        'length': turns,
-        'game': axelrod.Game()}
+
+#def iterate_plays(player, opponents, turns=200, repetitions=50, noise=0, aggClass=None):
+    #"""Runs many sequences of play to generate play data for computing the
+    #average score per turn for each opponent.
+
+    #For the given player and list of opponents, `repetitions` matches are played
+    #for each opponent and the results aggregated.
+
+    #"""
+    #tournament_attributes = {
+        #'length': turns,
+        #'game': axelrod.Game()}
+    #data = []
+    #for i, opponent in enumerate(opponents):
+        #aggregator = aggClass()
+        ## Check if the outcome will be deterministic. If so, only run one repetition
+        #if player.classifier['stochastic'] or opponent.classifier['stochastic'] or noise:
+            #repetitions_ = repetitions
+        #else:
+            #repetitions_ = 1
+        #for _ in range(repetitions_):
+            #player_ = player.clone()
+            #opponent_ = opponent.clone()
+            #player_.tournament_attributes = tournament_attributes
+            #opponent_.tournament_attributes = tournament_attributes
+
+            #for _ in range(turns):
+                #player_.play(opponent_, noise=noise)
+            #aggregator.add_data(player_.history, opponent_.history)
+
+            #player_.reset()
+            #opponent_.reset()
+        #averages = aggregator.normalize()
+        #data.append((i, averages))
+    #return data
+
+#def apply_aggregator(data, aggregator):
+    #for row in data:
+        #history1, history2 = unzip(row)
+        #aggregator.add_data(history1, history2)
+    #return aggregator.normalize()
+
+def aggregated_data(player, opponents, aggClass=None):
+    """Aggregates cached data for player versus every opponent for plotting."""
     data = []
     for i, opponent in enumerate(opponents):
         aggregator = aggClass()
-        if player.classifier['stochastic'] or opponent.classifier['stochastic'] or noise:
-            repetitions_ = repetitions
-        else:
-            repetitions_ = 1
-        for _ in range(repetitions_):
-            player_ = player.clone()
-            opponent_ = opponent.clone()
-            player_.tournament_attributes = tournament_attributes
-            opponent_.tournament_attributes = tournament_attributes
-
-            for _ in range(turns):
-                player_.play(opponent_, noise=noise)
-            aggregator.add_data(player_.history, opponent_.history)
-
-            player_.reset()
-            opponent_.reset()
+        match_data = load_match_csv(player, opponent)
+        for row in match_data:
+            # Produce two history lists
+            history1, history2 = unzip(row)
+            aggregator.add_data(history1, history2)
         averages = aggregator.normalize()
         data.append((i, averages))
     return data
 
+def aggregated_data_to_csv(player, opponents, aggClass=None):
+    """Aggregates cached data for player versus every opponent for plotting."""
+    data = []
+    for i, opponent in enumerate(opponents):
+        aggregator = aggClass()
+        match_data = load_match_csv(player, opponent)
+        for row in match_data:
+            # Produce two history lists
+            history1, history2 = unzip(row)
+            aggregator.add_data(history1, history2)
+        averages = aggregator.normalize()
+        data.append((i, averages))
+    return data
+
+    path = Path("csv") / "matches" / filename
+    with path.open('w') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in data:
+            csv_row = ["".join(element) for element in row]
+            writer.writerow(csv_row)
+
+
+
 def visualize_strategy(data, player, opponents, directory, turns=200,
                        repetitions=200, noise=0, cmap=None, sort=False,
                        vmin=0, vmax=1,):
-    """Plots the average cooperate rate or score per turn for `player` versus
+    """Plots the average (e.g.) cooperate rate or score per turn for `player` versus
     every opponent in `opponents`."""
     if not cmap:
         cmap = pyplot.get_cmap("RdBu")
@@ -176,11 +345,10 @@ def visualize_strategy(data, player, opponents, directory, turns=200,
     else:
         sort_order = range(len(opponents))
 
-    data = [x[-1] for x in data]
+    data = [x[-1] for x in data] # Toss the sorting index and just graph the value
     data = numpy.array(data)
 
-    player_name = str(player)
-    player_name = player_name.replace('/', '-')
+    player_name = normalized_name(player)
 
     # Plot the data in a pcolor colormap
     pyplot.clf()
@@ -193,10 +361,7 @@ def visualize_strategy(data, player, opponents, directory, turns=200,
     cbar = pyplot.colorbar(sm, ax=ax)
     pyplot.xlabel("Rounds")
 
-    output_directory = os.path.join("assets", directory)
-    ensure_directory(output_directory)
-
-    filename = os.path.join(output_directory, "%s.png" % (player_name,))
+    filename = os.path.join(directory, "%s.png" % (player_name,))
     pyplot.savefig(filename)
     pyplot.close(fig)
 
@@ -206,50 +371,87 @@ def make_figures(strategies, opponents, turns=200, repetitions=50,
     # Score heatmaps
     if function == 's':
         cmap = pyplot.get_cmap("autumn")
-        directory = "score_heatmaps"
-        #func = compute_score_data
+        directory = "score"
         vmin, vmax = game_extremes()
         aggClass = ScoreAggregator
     # Score Diff heatmaps
     elif function == 'sd':
         cmap = pyplot.get_cmap("autumn")
-        directory = "score_diff_heatmaps"
-        #func = compute_score_data
+        directory = "score_diff"
         vmin, vmax = game_extremes()
         aggClass = ScoreDiffAggregator
     # Cooperation heatmaps
     elif function == 'c':
         cmap = pyplot.get_cmap("RdBu")
-        directory = "cooperation_heatmaps"
-        #func = compute_cooperation_data
+        directory = "cooperation"
         vmin, vmax = None, None
         aggClass = CooperationAggregator
     # Opponent_cooperation_heatmaps
     elif function == 'oc':
         cmap = pyplot.get_cmap("RdBu")
-        directory = "opponent_cooperation_heatmaps"
-        #func = compute_cooperation_data
+        directory = "opponent_cooperation"
         vmin, vmax = None, None
         aggClass = OpponentCooperationAggregator
     else:
         raise ValueError("Invalid function option")
     if noise:
-        directory += "_noise"
+        directory += "-noisy"
+    path = Path("assets") / "heatmaps" / directory
 
-    for index, strategy in enumerate(strategies):
-        print(index, function, strategy)
-        data = iterate_plays(strategy, opponents, turns=turns, aggClass=aggClass,
-                             repetitions=repetitions, noise=noise)
-        visualize_strategy(data, strategy, opponents, directory, noise=noise,
+    for index, player in enumerate(strategies):
+        print(index, function, player)
+        data = aggregated_data(player, opponents, aggClass=aggClass)
+        #data = iterate_plays(strategy, opponents, turns=turns, aggClass=aggClass,
+                             #repetitions=repetitions, noise=noise)
+
+        visualize_strategy(data, player, opponents, directory=str(path), noise=noise,
                            cmap=cmap, vmin=vmin, vmax=vmax)
         matplotlib.pyplot.close("all")
 
+def init():
+    # Check python version
+    version = sys.version.split('.')
+    if (int(version[0]) == 2) or (int(version[0] == 3) and int(version[1]) < 4):
+        print("Python 3.4+ is required")
+        sys.exit()
+    # Make sure all the necessary paths exist
+    path = Path("assets")
+    ensure_directory(str(path))
+    path = path / "csv"
+    ensure_directory(str(path))
+    path = path / "matches"
+    ensure_directory(str(path))
+
+    path = Path("assets")
+    ensure_directory(str(path))
+    path = path / "heatmaps"
+    ensure_directory(str(path))
+    for sub in ["score", "score_diff", "cooperation", "opponent_cooperation"]:
+        path = Path("assets") / "heatmaps" / sub
+        ensure_directory(str(path))
+        path = Path("assets") / "heatmaps" / (sub + "-noisy")
+        ensure_directory(str(path))
+
+def generate_data(players):
+    save_all_match_results(players, turns=200, repetitions=1000)
+
+"""Todo:
+save aggregrated data to csv
+"""
+
 if __name__ == "__main__":
-    all_strategies = list(reversed(axelrod_strategies()))
-    strategies = list(all_strategies)
-    opponents = list(all_strategies)
+    init()
+    turns, repetitions, noise, function, gen_data = parse_args()
 
-    turns, repetitions, noise, function = parse_args()
+    # Grab the strategy lists from axelrod
+    players = list(reversed(axelrod_strategies()))
+    opponents = list(players)
 
-    make_figures(strategies, opponents, turns=turns, repetitions=repetitions,
+    # Generate the data?
+    if gen_data:
+        save_all_match_results(players, turns=200, repetitions=1000)
+        exit()
+
+    # Visualizations
+    make_figures(players, opponents, turns=turns, repetitions=repetitions,
                  noise=noise, function=function)
